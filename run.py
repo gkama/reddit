40237b74c94bda82777e3ffa96c2f0a7d905f75f
Georgi Kamacharov <gkamacharov@gmail.com>
2020-02-27 08:13:28 -0500

    add logzero

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from scripts.subreddit import *
+from logzero import logger
+
 
 def main(argv):
     script_name = argv[1]
+    logger.info("test")
     print("in in run.py")
     print("script name " + script_name)
     print(get_str())
