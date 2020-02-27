b314b6bb91adcf6b8a24629bd493c618bc7af32a
Georgi Kamacharov <gkamacharov@gmail.com>
2020-02-27 09:09:38 -0500

    update to add script input and script execution

--- run.py
+++ run.py
@@ -10,15 +10,19 @@ def main(argv):
         if argv[1:]:
             script_name = argv[1]
             if argv[2:]:
-                script_input = argv[2:]
+                script_inputs = argv[2:]
     except Exception as e:
         logger.error("couldn't find configuration for specified script")
         sys.exit()
 
+    logger.info("getting configuration for script={0}".format(script_name))
     config = Configuration()
     script_config = config.get_config(script_name)
-    
-    print(get_subreddit_data("personalfinance"))
+    json_config = config.get_json_config()
+
+    if script_name == "subreddit":
+        subr = script_inputs[0]
+        logger.info("running script={0} with input={1}".format(script_name, script_inputs[0]))
 
 
 if __name__ == "__main__":
