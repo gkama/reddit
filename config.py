1f7cdc357caefec920498b90ed59256986c1f2fb
Georgi Kamacharov <gkamacharov@gmail.com>
2020-02-27 09:09:06 -0500

    update to get json config

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import yaml
 
 from logzero import logger
@@ -6,15 +7,24 @@ from logzero import logger
 
 class Configuration(object):
     def __init__(self):
-        self.config_file_path = ".\\config.yaml"
+        self.config_json_file_path = ".\\config.json"
+        self.config_yaml_file_path = ".\\config.yaml"
 
     
     def get_config(self, script_name):
-        with open(self.config_file_path, "r") as f:
+        with open(self.config_yaml_file_path, "r") as f:
             try:
                 config_yaml = yaml.safe_load(f)
                 for script_config in config_yaml["scripts"]:
                     if script_config["name"] == script_name:
                         return script_config
             except yaml.YAMLError as e:
-                logger.error(e)
+                logger.exception(e)
+
+    
+    def get_json_config(self):
+        with open(self.config_json_file_path, "r") as f:
+            try:
+                return json.loads(f.read())
+            except Exception as e:
+                logger.exception(e)
