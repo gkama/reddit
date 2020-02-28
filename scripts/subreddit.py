1e946ceecd392bb456af8cca682acaf3c91de83d
Georgi Kamacharov <gkamacharov@gmail.com>
2020-02-28 09:09:34 -0500

    update config to move to scripts folder

--- scripts/subreddit.py
+++ scripts/subreddit.py
@@ -1,12 +1,19 @@
+import os
 import sys
 import requests
 
 from logzero import logger
+from config import Configuration
+
+
+config = Configuration()
+script_name = sys.argv[0]
+script_config = config.get_config(script_name)
 
 
 def get_subreddit_data(subreddit_name):
     try:
-        response = requests.get("https://www.reddit.com/r/{0}.json".format(subreddit_name))
+        response = requests.get("https://{0}/r/{1}.json".format(script_config["host"], subreddit_name))
         return response.text
     except Exception as e:
         logger.error(e)
